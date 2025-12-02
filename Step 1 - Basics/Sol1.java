0998920e870f439a29fbd84e83b8c20752f8b053
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-02 12:24:26 +0530

    Lec 1 : User Input / Output

---
+++ Step 1 - Basics/Sol1.java
@@ -0,0 +1,22 @@
+// User Input & Output
+
+import java.util.*;
+
+public class Sol1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        System.out.println(a);
+    }
+}
+
+// import java.util.*;
+
+// class Solution {
+//     public void printNumber() {
+//         Scanner in = new Scanner(System.in);
+//         int a = in.nextInt();
+//         System.out.print(a);
+//     }
+// }
