18851938a3eccbd4665cd742b27be9845c89c682
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 11:13:09 +0530

    User Input & Output

--- Step 1 - Basics/A1.java
+++ Step 1 - Basics/A1.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class Sol1 {
+public class A1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
