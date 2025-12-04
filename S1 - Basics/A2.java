a7eefa6af78ffe8253ab045322d5793e610c0b38
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 11:47:13 +0530

    If ElseIf

--- S1 - Basics/A2.java
+++ S1 - Basics/A2.java
@@ -1,7 +1,7 @@
 // If ElseIf
 // Given marks of a student, print on the screen:
 
-// Grade A if marks >= 90
+// Grade A if marks >= 90 
 // Grade B if marks >= 70
 // Grade C if marks >= 50
 // Grade D if marks >= 35
