3b41f6ce767a3de114c0e9252ccf5a031c9247b4
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 12:44:58 +0530

    Duplicate Detection using Hashing

--- S5 - Hashing/A5.java
+++ S5 - Hashing/A5.java
@@ -1,5 +1,30 @@
+// Duplicate Detection using Hashing
+
 import java.util.*;
 
 public class A5 {
-    
+    public static void main(String[] args) {    
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        boolean result = solve(n, in);
+        System.out.println(result);
+    }
+
+    public static boolean solve(int n, Scanner in) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+
+            if (set.contains(num)) {
+                return true;   
+            }
+            else {
+                set.add(num);
+            }
+        }
+
+        return false; 
+    }
 }
