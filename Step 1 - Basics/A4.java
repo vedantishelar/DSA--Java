5983c359b34608bd2cb2a84d663b4f860517daca
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 11:19:30 +0530

    Count all Digits of a Number

--- Step 1 - Basics/A4.java
+++ Step 1 - Basics/A4.java
@@ -5,7 +5,7 @@
 
 import java.util.*;
 
-public class Sol4 {
+public class A4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
