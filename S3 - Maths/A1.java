b817d08a30b498bb4ec40003b859d0a85cd78473
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 11:49:41 +0530

    Count all Digits of a Number

--- S3 - Maths/A1.java
+++ S3 - Maths/A1.java
@@ -5,7 +5,7 @@
 
 import java.util.*;
 
-public class A4 {
+public class A1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
