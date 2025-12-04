7ac6061dcc19aa7ac00f37c4c33a76c2df78d3ff
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 12:12:07 +0530

    Check for Prime

--- S3 - Maths/A6.java
+++ S3 - Maths/A6.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class A9 {
+public class A6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
