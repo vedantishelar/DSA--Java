2374b5f3881762d28ed76e0fa277271e41677c1d
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 12:14:36 +0530

    Print 1 to N using recursion

--- S4 - Recursion/A1.java
+++ S4 - Recursion/A1.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class B1 {
+public class A1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
