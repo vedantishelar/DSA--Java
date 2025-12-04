282ca8a6238b2691a3e89c3de33599d8a8ab3609
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-12-04 11:25:02 +0530

    Armstrong Numbers

--- Step 1 - Basics/A8.java
+++ Step 1 - Basics/A8.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class Sol8 {
+public class A8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
