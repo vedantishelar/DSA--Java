2c7d4db9930fe9c0ba36115bcd8024cc38121d59
Vedanti Shelar <shelarvedanti485@gmail.com>
2026-01-18 23:11:39 +0530

    Warehouse Load Balancer

--- S7-NQT-Array/A8.java
+++ S7-NQT-Array/A8.java
@@ -1,6 +1,21 @@
+// 📦 Problem: Warehouse Load Balancer
+
+// A logistics company manages a warehouse with N containers placed in a single row.
+// Each container has a load value representing its weight.
+
+// Due to safety rules, the warehouse manager wants to identify a continuous block of containers such that:
+
+// The total load of the selected block is maximum
+
+// At least one container must be selected
+
+// Containers must be selected in order (continuous)
+
+// Your task is to help the manager determine the maximum total load that can be achieved by selecting such a block.
+
 import java.util.*;
 
-public class WarehouseLoadBalancer {
+public class A8 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
